5b6a47b627a5c5ee8112ed36a5d37fd87cec62c6
NiRuinan <niruinan@foxmail.com>
2026-09-23 21:25:25 +0800

    Refactor chipseq.sh to focus on bowtie2_self
    Removed bowtie2_wild_boar section and adjusted parameters for peak calling.

--- 03.chipseq/chipseq.sh
+++ 03.chipseq/chipseq.sh
@@ -1,32 +1,5 @@
 #!/bin/bash
 
-# bowtie2_wild_boar
-for i in 1 2 3 "in"
-do
-  bowtie2 -p 32 --very-sensitive --no-mixed --no-discordant -k 10 \
-    -x wild_boar.fa \
-    -1 sample_${i}_clean_1.fastq.gz \
-    -2 sample_${i}_clean_2.fastq.gz \
-  | samtools sort -O bam -@ 32 -o sample.wild_boar_${i}.bam
-done
-
-for i in 1 2 3 "in"
-do
-  picard MarkDuplicates \
-    --INPUT sample.wild_boar_${i}.bam \
-    --OUTPUT sample.wild_boar_${i}.rmdup.bam \
-    --METRICS_FILE sample.wild_boar_${i}.dup_metrics.txt \
-    --REMOVE_DUPLICATES true
-
-  samtools index sample.wild_boar_${i}.rmdup.bam
-
-  bamCoverage \
-    --bam sample.wild_boar_${i}.rmdup.bam \
-    --outFileName sample.wild_boar_${i}.rmdup.bw \
-    --outFileFormat bigwig \
-    --binSize 5 -p 32
-done
-
 # bowtie2_self
 for i in 1 2 3 "in"
 do
